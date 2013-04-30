98cd6c4a4ded84a4ce9279aca3d5cb920234f117
Francisco Souza <f@souza.cc>
2013-04-29 22:55:31 -0300

    code: adicionando alguns exemplos elementares

---
+++ code/element.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sample1() {
+	var name string
+	name = "Francisco"
+	fmt.Println(name)
+}
+
+func sample2() {
+	var name string = "Francisco"
+	fmt.Println(name)
+}
+
+func sample3() {
+	var name = "Francisco"
+	fmt.Println(name)
+}
+
+func sample4() {
+	name := "Francisco"
+	fmt.Println(name)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	for {
+		fmt.Println("Looping forever")
+		break // not really
+	}
+}
