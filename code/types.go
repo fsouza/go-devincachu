2e650341347b250399ac8ea7552f8331eab50945
Francisco Souza <f@souza.cc>
2013-05-01 17:11:58 -0300

    code: exemplo com tipos e métodos

---
+++ code/types.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Name  string
+	Birth time.Time
+}
+
+func (p *Person) Age() int {
+	difference := time.Now().Sub(p.Birth)
+	return int(difference / (365 * 24 * time.Hour))
+}
+
+type MyString string
+
+type MyInt int
+
+func main() {
+	p := Person{
+		Name: "Francisco",
+		Birth: time.Date(1989, 2, 16, 0, 0, 0, 0, time.Local),
+	}
+	fmt.Println(p.Age())
+}
